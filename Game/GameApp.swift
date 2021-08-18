de5bcba5bb554d1a6fab5cfc28c549485e4a329e
keva damar galih <kevadamarg@gmail.com>
2021-08-18 14:32:19 +0700

    Initial Commit

---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by keva on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
